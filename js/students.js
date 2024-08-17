a3308bdc2dfd0482b1552fcc18ecad99cb5339f1
faysal2525 <faisal_2525@outlook.com>
2024-08-18 01:47:27 +0500

    Initial commit

---
+++ js/students.js
@@ -0,0 +1,47 @@
+const students = [
+
+  {
+  
+  name: "John Doe",
+  
+  semester: 3,
+  
+  father_name:"Doe",
+  
+  isFeePaid:true,
+  
+  courses: ["Mathematics", "Physics", "Computer" ,
+  
+  "Science"]
+  
+  },
+  
+  {
+  
+  name: "Jane Smith",
+  
+  father_name:"Smith",
+  
+  semester: 2,
+  
+  isFeePaid:true,
+  
+  courses: ["Biology", "Chemistry", "English"]
+  
+  },
+  
+  {
+  
+  name: "Emily Johnson",
+  
+  father_name: "Johnson",
+  
+  semester: 4,
+  
+  isFeePaid:false,
+  
+  courses: ["Economics", "Statistics", "Marketing"]
+  
+  }
+  
+  ];
